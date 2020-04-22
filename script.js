0af605313d70fed344b1791956698b848be460c1
vladimir <vladimirleonov789@gmail.com>
2020-04-22 20:15:11 +0300

    finished lesson 9

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let citiesId = 'cities';
 let hobiesId = 'hobies';
 let wrapperId = 'wrapper';
 let imgId = 'img';
+let listId = 'list';
 
 let firstNameEl = document.getElementById(firstNameId);
 let lastNameEl = document.getElementById(lastNameId);
@@ -13,6 +14,18 @@ let citiesEl = document.getElementById(citiesId);
 let hobiesEl = document.getElementById(hobiesId);
 let wrapperEl = document.getElementById(wrapperId);
 let imgEl = document.getElementById(imgId);
+let listEl = document.getElementById(listId);
+
+// firstNameEl.value = 'Misha';
+firstNameEl.setAttribute('value', 'Dany');
+firstNameEl.className = 'firstName error-input';
+imgEl.title = 'I am img';
+citiesEl.value = 'Moskcow';
+hobiesEl.value = 'I have not interest';
+wrapperEl.innerHTML = '<ul id=\'list\'><li>1</li><li>2</li><li>3</li></ul>';
+
+//error becouse пытается обратится через переменную до изменения .wrapper
+listEl.className = 'list';
 
 //innerHTML!=value
 // window.alert(firstNameEl.value);
@@ -26,3 +39,5 @@ let imgEl = document.getElementById(imgId);
 // window.alert(hobiesEl.value);
 // window.alert(wrapperEl.value);
 // window.alert(imgEl.alt);
+
+// window.alert(firstNameEl.className);
