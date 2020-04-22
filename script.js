0bc6038dbee3420722746b3258ce0a6a2f1781d4
vladimir <vladimirleonov789@gmail.com>
2020-04-22 19:37:45 +0300

    finished lesson 8.2

---
+++ script.js
@@ -0,0 +1,28 @@
+let firstNameId = 'first-name';
+let lastNameId = 'last-name';
+let addressId = 'address';
+let citiesId = 'cities';
+let hobiesId = 'hobies';
+let wrapperId = 'wrapper';
+let imgId = 'img';
+
+let firstNameEl = document.getElementById(firstNameId);
+let lastNameEl = document.getElementById(lastNameId);
+let addressEl = document.getElementById(addressId);
+let citiesEl = document.getElementById(citiesId);
+let hobiesEl = document.getElementById(hobiesId);
+let wrapperEl = document.getElementById(wrapperId);
+let imgEl = document.getElementById(imgId);
+
+//innerHTML!=value
+// window.alert(firstNameEl.value);
+// window.alert(lastNameEl.value);
+// window.alert(addressEl.value);
+
+//innerHTML!=value
+// window.alert(citiesEl.value);
+
+//innerHTML=value
+// window.alert(hobiesEl.value);
+// window.alert(wrapperEl.value);
+// window.alert(imgEl.alt);
