6f6d113e7cd6b8ad8ba8f0906a4a06efc4dc0f61
vladimir <vladimirleonov789@gmail.com>
2020-04-23 18:05:45 +0300

    finished lesson 13

--- script.js
+++ script.js
@@ -8,61 +8,20 @@
 // let addressEl = document.getElementById(addressId);
 // console.logEl(addressEl.value)
 
-function consoleLogValue(param) {
+function getEl(param) {
    let id = param;
    let el = document.getElementById(id);
-   console.log(el.value);
+   return el;
 }
-consoleLogValue('first-name');
-consoleLogValue('last-name');
-consoleLogValue('address');
 
-// let citiesId = 'cities';
-// let hobiesId = 'hobies';
-// let wrapperId = 'wrapper';
-// let imgId = 'img';
-// let listId = 'list';
+let firstNameId = 'first-name';
+let lastNameId = 'last-name';
+let addressId = 'address';
 
+let firstNameEl = getEl(firstNameId);
+getEl(lastNameId);
+getEl(addressId);
 
+console.log(firstNameEl.value);
 
-
-// let citiesEl = document.getElementById(citiesId);
-// let hobiesEl = document.getElementById(hobiesId);
-// let wrapperEl = document.getElementById(wrapperId);
-// let imgEl = document.getElementById(imgId);
-// let listEl = document.getElementById(listId);
-
-// firstNameEl.value = 'Misha';
-// firstNameEl.setAttribute('value', 'Dany');
-// firstNameEl.className = 'firstName error-input';
-// imgEl.title = 'I am img';
-// citiesEl.value = 'Moskcow';
-// hobiesEl.value = 'I have not interest';
-// wrapperEl.innerHTML = '<ul id=\'list\'><li>1</li><li>2</li><li>3</li></ul>';
-
-//error becouse пытается обратится через переменную до изменения .wrapper
-// listEl.className = 'list';
-
-//innerHTML!=value
-// window.alert(firstNameEl.value);
-// window.alert(lastNameEl.value);
-// window.alert(addressEl.value);
-
-//innerHTML!=value
-// window.alert(citiesEl.value);
-
-//innerHTML=value
-// window.alert(hobiesEl.value);
-// window.alert(wrapperEl.value);
-// window.alert(imgEl.alt);
-
-// window.alert(firstNameEl.className);
-
-// function alertValue(firstNameEl, lastNameEl, addressEl) {
-//    window.alert(firstNameEl.value);
-//    window.alert(lastNameEl.value);
-//    window.alert(addressEl.value);
-// }
-
-// alertValue(firstNameEl, lastNameEl, addressEl);
 
