3ef6b2fe0ecf576013e9d4d2fc6dfc4126329934
Admin <vladimirleonov789@gmail.com>
2020-05-01 20:59:44 +0300

    finished lesson26 example konstruktor

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-let slider1 = sliderFactory.createNewSlider();//в этих переменных находятся объекты
-let slider2 = sliderFactory.createNewSlider();// 
+// let slider1 = sliderFactory.createNewSlider();//в этих переменных находятся объекты
+// let slider2 = sliderFactory.createNewSlider();// 
 
-slider1.start('slider1');
-slider2.start('slider2');
+// slider1.start('slider1');
+// slider2.start('slider2');
 
+function Dog(name) {
+   //let this = {};
+   this.myName = name;
+   this.voice = function () {
+      console.log('Hey, my name is ' + this.myName);
+   }
+   //return this;
+};
 
+let dog2 = {
+   myName: 'chihuahua',
+   voice: function () {
+      console.log('hey hey');
+   }
+};
 
-
+// Dog();
+// dog1 = new Dog();
+// dog2.voice();
+dog3 = new Dog('qqqqq');
+dog4 = new Dog('wwwww');
 
 //this ссылается на слайдер, поэтому в start this будет ссылаться на слайдер
 
